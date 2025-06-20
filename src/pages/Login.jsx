f79f54e3ba79be107cfa7f8abe309737e4e05e7b
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-06-20 15:39:28 +0600

    create login page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
+  const handleGoogleSignIn = () => {}
+
+  return (
+    <div className="flex justify-center min-h-[calc(100vh-330px)] items-center">
+      <div
+        className={"card w-full max-w-sm shrink-0 shadow-2xl py-5 bg-base-100"}
+      >
+        <h2 className={"font-semibold text-2xl text-center text-red-700"}>
+          Login your account
+        </h2>
+        <form onSubmit={handleLogin} className="card-body">
+          <fieldset className="fieldset">
+            {/* email  */}
+            <label className="label">Email</label>
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+              //   onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            {/* passowrd  */}
+            <label className="label">Password</label>
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+              required
+            />
+
+            {/* <div>
+              <Link to={`/auth/forget-password?email=${encodeURIComponent(email)}`} className="link link-hover">Forgot password?</Link>
+            </div> */}
+
+            {/* {error && (
+              <p className="text-red-500 text-xs font-semibold text-center mt-3">
+                {error}
+              </p>
+            )} */}
+
+            <button type="submit" className="btn bg-red-700 text-white mt-4">
+              Login
+            </button>
+
+            <button
+              onClick={handleGoogleSignIn}
+              className="btn bg-white text-black border-red-700 mt-3"
+            >
+              <svg
+                aria-label="Google logo"
+                width="16"
+                height="16"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <g>
+                  <path d="m0 0H512V512H0" fill="#fff"></path>
+                  <path
+                    fill="#34a853"
+                    d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                  ></path>
+                  <path
+                    fill="#4285f4"
+                    d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                  ></path>
+                  <path
+                    fill="#fbbc02"
+                    d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                  ></path>
+                  <path
+                    fill="#ea4335"
+                    d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                  ></path>
+                </g>
+              </svg>
+              Login with Google
+            </button>
+
+            <p className="font-semibold text-sm text-center pt-5 ">
+              Don’t Have An Account?{" "}
+              <Link className="text-red-700" to="/register">
+                Register
+              </Link>
+            </p>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
