3ad81f72f8b11d0bcf1a6ef04df0fe71f9ff54f6
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-06-20 15:38:38 +0600

    create routes for the components

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      }
+    ]
   },
 ]);
 
