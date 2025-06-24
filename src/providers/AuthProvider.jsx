c597c3702c1a2562c3b2dabd1d4244173458501e
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-06-24 16:45:59 +0600

    feat: enhance AuthProvider with user state management and data fetching

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../firebase/firebase.init';
 
 const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const createUser = (email, password) => {
       return createUserWithEmailAndPassword(auth, email, password);
@@ -17,7 +20,39 @@ const AuthProvider = ({ children }) => {
       return signOut(auth);
     };
 
+    const fetchUserData = (email) => {
+      fetch(`http://localhost:3000/users/${email}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setUser(data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoading(false);
+        });
+    };
+
+    useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+
+        if (currentUser?.email) {
+          fetchUserData(currentUser.email);
+        } else {
+          setUser(null);
+        }
+        setLoading(false);
+      });
+      return () => {
+        unsubscribe();
+      };
+    }, []);
+
     const authData = {
+        user,
+        setUser,
+        loading,
         createUser,
         loginUser,
         logOut
