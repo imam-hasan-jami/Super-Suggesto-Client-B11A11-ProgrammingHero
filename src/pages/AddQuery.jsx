1ca4ad00f452b6704b888ee7787bd1ccc4ce677a
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-06-25 16:33:48 +0600

    added functionality to add query to the db

--- src/pages/AddQuery.jsx
+++ src/pages/AddQuery.jsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { use } from "react";
+import { AuthContext } from '../providers/AuthContext';
+import Swal from "sweetalert2";
 
 const AddQuery = () => {
+
+  const { user } = use(AuthContext);
+
+  const handleAddQuery = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const newQuery = Object.fromEntries(formData.entries());
+
+    newQuery.userEmail = user?.email || "";
+    newQuery.userName = user?.name || "";
+    newQuery.userPhotoURL = user?.photoURL || "";
+    newQuery.dateTime = new Date().toISOString();
+    newQuery.recommendationCount = 0;
+
+    fetch("http://localhost:3000/queries", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newQuery),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `Query Added successfully.`,
+            showConfirmButton: false,
+            timer: 2000,
+          });
+          // form.reset();
+        }
+      })
+      .catch((error) => {
+        console.error("Error adding plant:", error);
+      });
+  };
+
   return (
     <div>
       <div className="my-10">
         <h1 className="text-center font-bold text-3xl my-7">Add Query</h1>
 
-        <form>
+        <form onSubmit={handleAddQuery}>
           <fieldset className="fieldset grid lg:grid-cols-1 gap-6 bg-base-200 border-base-300 rounded-box w-10/12 lg:w-5/12 mx-auto border p-4 lg:p-8">
             <div>
               <div className="flex flex-col">
