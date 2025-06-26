c58af394f0d6902d614c19be6e5b749e8e26fc11
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2025-06-26 23:59:21 +0600

    added route for MyQueries and created MyQueries component

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import AddQuery from "../pages/AddQuery";
+import MyQueries from "../pages/MyQueries";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: "/add-query",
         Component: AddQuery,
+      },
+      {
+        path: "/my-queries",
+        Component: MyQueries,
       }
     ]
   },
